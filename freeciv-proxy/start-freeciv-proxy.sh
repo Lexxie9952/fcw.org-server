687e980577a680f2be7b1432fe83827b57b7bdff
Freeciv <freeciv@play.freecivweb.org>
2019-03-20 20:02:25 +0100

    logging

--- freeciv-proxy/start-freeciv-proxy.sh
+++ freeciv-proxy/start-freeciv-proxy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Starts freeciv-proxy.
 
-until python3 ../freeciv-proxy/freeciv-proxy.py "${1}" > "../logs/freeciv-proxy-${3}.log" 2>&1 ; do
-  echo "Freeciv-proxy crashed with exit code $?. Restarting" >&2
+until python3 ../freeciv-proxy/freeciv-proxy.py "${1}" > "../logs/freeciv-proxy-${1}.log" 2>&1 ; do
+  echo $(date) "Freeciv-proxy ${1} crashed with exit code $?. Restarting" >&2
   sleep 10
 done
