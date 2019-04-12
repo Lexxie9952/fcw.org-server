8ccd9f430c03378a9091a73ffc5936fa3b294afe
Yolo <yoloswag6@protonmail.com>
2019-04-12 07:19:27 +0200

    Update flip-google-authorization.sh so it doesn't produce (harmless) .rej files

--- scripts/flip-google-authorization.sh
+++ scripts/flip-google-authorization.sh
@@ -1,4 +1,4 @@
-patch -p1 -u -R -s -d .. <Remove-Google-Authorization-in-Longturn.patch >/dev/null 2>/dev/null
+patch -p1 -u -R -s --dry-run -d .. <Remove-Google-Authorization-in-Longturn.patch >/dev/null 2>/dev/null
 if [ $? -ne 0 ];then
     patch -p1 -u -s -d .. <Remove-Google-Authorization-in-Longturn.patch
     if [ $? -eq 0 ];then
@@ -11,9 +11,15 @@ if [ $? -ne 0 ];then
         exit 1
     fi
 else
-  printf "Sucessfully added Google authorization back in!\n\nfreeciv-web/src/main/webapp/javascript/pregame.js\nfreeciv-proxy/freeciv-proxy.py\nand freeciv-web/src/main/webapp/javascript/clinet.js have been made committable again.\n\n"
-  git update-index --no-assume-unchanged ../freeciv-web/src/main/webapp/javascript/pregame.js
-  git update-index --no-assume-unchanged ../freeciv-proxy/freeciv-proxy.py
-  git update-index --no-assume-unchanged ../freeciv-web/src/main/webapp/javascript/clinet.js
+  patch -p1 -u -R -s -d .. <Remove-Google-Authorization-in-Longturn.patch
+  if [ $? -eq 0 ];then
+    printf "Sucessfully added Google authorization back in!\n\nfreeciv-web/src/main/webapp/javascript/pregame.js\nfreeciv-proxy/freeciv-proxy.py\nand freeciv-web/src/main/webapp/javascript/clinet.js have been made committable again.\n\n"
+    git update-index --no-assume-unchanged ../freeciv-web/src/main/webapp/javascript/pregame.js
+    git update-index --no-assume-unchanged ../freeciv-proxy/freeciv-proxy.py
+    git update-index --no-assume-unchanged ../freeciv-web/src/main/webapp/javascript/clinet.js
+  else
+    echo "Failed to restore Google authorization!"
+    exit 1
+  fi
 fi
 echo "You need to rebuild freeciv-web's javascript files for these changes to take effect (freeciv-web/build-js.sh)"
