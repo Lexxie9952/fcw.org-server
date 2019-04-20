6611d604adaa394489059d924e2d9dd03a0fb904
Yolo <yoloswag6@protonmail.com>
2019-04-20 09:29:09 +0200

    Add a convenient way to test rulesets on vagrant

---
+++ scripts/copy-ruleset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" != "" ]; then
+    cp /vagrant/freeciv/freeciv/data/$1.serv ~/freeciv/share/freeciv/$1.serv
+    cp -r /vagrant/freeciv/freeciv/data/$1 ~/freeciv/share/freeciv
+else
+    echo "Please specify the ruleset to copy (for example ./copy-ruleset.sh mp2)"
+fi
