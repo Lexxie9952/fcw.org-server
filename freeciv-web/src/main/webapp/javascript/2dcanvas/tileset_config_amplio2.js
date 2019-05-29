32f8f61c18f1d3d48b4dc4cf6ef8394159c53070
Lexxie <elisabeth.alexandra.lorraine@gmail.com>
2019-05-28 20:09:01 -0700

    Update tileset_config_amplio2.js

--- freeciv-web/src/main/webapp/javascript/2dcanvas/tileset_config_amplio2.js
+++ freeciv-web/src/main/webapp/javascript/2dcanvas/tileset_config_amplio2.js
@@ -64,7 +64,7 @@ var unit_activity_offset_y = 25;
 
 // offset the units by this amount when drawing units
 var unit_offset_x = 16;
-var unit_offset_y = 18;
+var unit_offset_y = 10;
 
 // Enable citybar
 var is_full_citybar = 1;
