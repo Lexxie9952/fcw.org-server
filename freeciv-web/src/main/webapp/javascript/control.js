425956b111c2e692b0807345b71283c5ae2bef7e
Lexxie <elisabeth.alexandra.lorraine@gmail.com>
2019-05-31 02:42:41 -0700

    Update control.js

--- freeciv-web/src/main/webapp/javascript/control.js
+++ freeciv-web/src/main/webapp/javascript/control.js
@@ -3153,7 +3153,7 @@ function update_active_units_dialog()
 	   + " style='margin-right:1px; background: transparent url(" 
            + sprite['image-src'] +
            ");background-position:-" + sprite['tileset-x'] + "px -" + sprite['tileset-y']
-           + "px;  background-size: 64px 48px; width:64px;height:48px;'"   // force everything to 64x48 including oversize units (Lexxie)
+           + "px; width:64px;height:48px;'"   // force everything to 64x48 including oversize units (Lexxie)
 //         + "px;  width: " + sprite['width'] + "px;height: " + sprite['height'] + "px;'"   previous line
            + "'></div></div>";                                 // changed margin-right to 1px, was defaulting to 5px (Lexxie)
     width = 64; // = sprite['width'];    // they are all 64 except oversize which we want to FORCE to 64 anyway to avoid buggy display (Lexxie)
