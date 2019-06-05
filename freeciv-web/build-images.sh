a7aec0b075c911549158eaf1882eb758194fe8d6
Lexxie <elisabeth.alexandra.lorraine@gmail.com>
2019-06-05 11:44:53 -0700

    Create build-images.sh
    Script in same dir as other build scripts, for more convenience to not type out super long command-line for image extraction.

---
+++ freeciv-web/build-images.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# builds Freeciv-web, copies the war file to Tomcat and builds the selected rulesets.
+
+BATCH_MODE=""
+RULESETS=(mpplus mp2 classic multiplayer mp2sandbox)
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -B) BATCH_MODE="-B"; shift;;
+    *) echo "Unrecognized argument: $1"; shift;;
+  esac
+done
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+
+TOMCATDIR="/var/lib/tomcat8"
+WEBAPP_DIR="${DIR}/target/freeciv-web"
+
+printf "\nRebuilding FCW images...\n"
+~/freeciv-web/scripts/freeciv-img-extract/sync.sh -f ~/freeciv-web/freeciv/freeciv -o ~/freeciv-web/freeciv-web/src/derived/webapp      
+   
