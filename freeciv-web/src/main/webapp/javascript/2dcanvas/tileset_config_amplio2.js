2c9e67f47169acdba948e0cabaae69047268e3ff
Lexxie <elisabeth.alexandra.lorraine@gmail.com>
2019-06-07 14:55:32 -0700

    adjust

--- freeciv-web/src/main/webapp/javascript/2dcanvas/tileset_config_amplio2.js
+++ freeciv-web/src/main/webapp/javascript/2dcanvas/tileset_config_amplio2.js
@@ -72,8 +72,8 @@ var unit_offset_adj_x = -9;      //-3
 var unit_offset_adj_y = -7;      //-3 
 
 // new vars for adjusting city tile position only:
-var city_offset_x = 5;
-var city_offset_y = 10; 
+var city_offset_x = 6;
+var city_offset_y = 5; 
 
 // Enable citybar
 var is_full_citybar = 1;
