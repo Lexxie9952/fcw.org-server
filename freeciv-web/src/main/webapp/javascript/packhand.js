59e0036fffaf9db39f09ac88b8d9a5ad0b480378
Lexxie <elisabeth.alexandra.lorraine@gmail.com>
2019-06-15 13:18:57 -0700

    Attempt to fix unresolved combat situation
    combat_rounds can mean neither unit dies
    it was not showing updated info on the units after the combat, but pre-combat info instead
    attempt to force an update to the unit info, test #1

--- freeciv-web/src/main/webapp/javascript/packhand.js
+++ freeciv-web/src/main/webapp/javascript/packhand.js
@@ -837,6 +837,23 @@ function handle_unit_combat_info(packet)
       explosion_anim_map[defender['tile']] = 25;
       play_combat_sound(attacker); //defender lost, player attacker combat sound
     }
+    // special new case in some rulesets: both units survive due to combat rounds/bombardment/etc.
+    if (defender_hp > 0 && is_unit_visible(defender) && attacker_hp > 0 && is_unit_visible(attacker))
+    {
+      play_combat_sound(defender); //play both sounds even though neither lost -- seems the only
+      play_combat_sound(attacker); //way to indicate no losses until we get a special sound for this case
+      
+      // it was not sending a message after battle, so inject one here:
+      var special_messages = [""];
+      special_messages[0] = "A valiant battle with no winner! Attacker retains "+attacker_hp+"hp. Defender retains "+defender_hp+"hp.";
+      update_chatbox(special_messages);   // this function wants an array
+      // hack -- it doesn't redraw the map/unit to show its new health status, but sentry appears the only way to get the new info
+      // TO DO -- figure out a real way to get updated info and redraw for both units
+      request_new_unit_activity(attacker, ACTIVITY_SENTRY, EXTRA_NONE);
+      request_new_unit_activity(defender, ACTIVITY_SENTRY, EXTRA_NONE);
+      update_map_canvas_full();
+      
+    } 
   }
 
 
