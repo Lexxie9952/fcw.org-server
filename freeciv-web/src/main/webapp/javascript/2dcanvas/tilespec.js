2673f6c2c7ea32250907f4871b0da71732ee4efd
Lexxie <elisabeth.alexandra.lorraine@gmail.com>
2019-07-01 00:11:40 -0700

    test

--- freeciv-web/src/main/webapp/javascript/2dcanvas/tilespec.js
+++ freeciv-web/src/main/webapp/javascript/2dcanvas/tilespec.js
@@ -905,8 +905,10 @@ function get_border_line_sprites(ptile)
         && ptile['owner'] != 255 /* 255 is a special constant indicating that the tile is not owned by anyone. */
         && players[ptile['owner']] != null) {
       var pnation = nations[players[ptile['owner']]['nation']];
+      //result.push({"key" : "border", "dir" : dir,
+      //             "color": pnation['color']});
       result.push({"key" : "border", "dir" : dir,
-                   "color": pnation['color']});
+                      "color": "rgb(0,0,0)" });
 
       console.log(pnation['color']);             
     }
