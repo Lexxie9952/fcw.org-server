26e17daea0330e3ac2aa20cbc37486c057c86ec3
Lexxie <elisabeth.alexandra.lorraine@gmail.com>
2019-07-01 00:38:35 -0700

    test

--- freeciv-web/src/main/webapp/javascript/2dcanvas/tilespec.js
+++ freeciv-web/src/main/webapp/javascript/2dcanvas/tilespec.js
@@ -930,7 +930,7 @@ function get_grid_line_sprites(ptile)
 
     if (checktile != null)
       result.push({"key" : "border", "dir" : dir,
-                      "color": "rgba(0,0,0,0.5)" });
+                      "color": "rgba(0,0,0,0.2)" });
   }
 
   return result;
