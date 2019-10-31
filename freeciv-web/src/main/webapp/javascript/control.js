06644d46eb4d578c6d150d7b14a3a969b14f890e
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2019-10-31 04:15:07 -0700

    Fix "Select all not-same type on tile"
    in cases where focused unit was not first unit in stack but distinct from focused unit, select all not-same-type would miss this unit

--- freeciv-web/src/main/webapp/javascript/control.js
+++ freeciv-web/src/main/webapp/javascript/control.js
@@ -3596,7 +3596,7 @@ function key_select_different_units_on_tile()
     var ptype = punit['type'];
 
     punits = tile_units(ptile);
-    for (var i=1; i<punits.length; i++) { // start at [1] so [0] isn't added twice
+    for (var i=0; i<punits.length; i++) { 
       if ( unit_types[punits[i]['type']]['name'] != unit_types[ptype]['name'] ) {
           // make sure it's not already in selection before adding it to selection:
           var index = current_focus.findIndex(x => x.id==punits[i].id);
