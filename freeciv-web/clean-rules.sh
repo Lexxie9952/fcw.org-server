c3567e5c39bba4208e91ece2fc497f46023e4d81
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2020-12-08 14:51:50 -0800

    mp2-brava
    Explorer:  vision goes from sqrt of 4 to 5
    Well-Digger:  use punctuation to clarify textual ambiguity

--- freeciv-web/clean-rules.sh
+++ freeciv-web/clean-rules.sh
@@ -1,7 +1,7 @@
 11#!/bin/bash
 # builds Freeciv-web, copies the war file to Tomcat and builds the selected rulesets.
 
-RULESETS=(classic civ2civ3 maptest multiplayer mpplus mp2sandbox mp2 ag ag2)
+RULESETS=(classic civ2civ3 maptest multiplayer mpplus mp2sandbox mp2 ag ag2 mp2-brava)
 TOPDIR="$( cd ../"$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 printf "\nThis overwrites auto-generated manual for units file, ag7.html, with\n"
