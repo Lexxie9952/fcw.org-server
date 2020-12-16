5117a80930bc1e44cb1ddf1d6bcf2b8b95dc665b
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2020-12-15 20:27:56 -0800

    Update build scripts for new rulesets

--- freeciv-web/clean-rules.sh
+++ freeciv-web/clean-rules.sh
@@ -1,7 +1,7 @@
 11#!/bin/bash
 # builds Freeciv-web, copies the war file to Tomcat and builds the selected rulesets.
 
-RULESETS=(classic civ2civ3 maptest multiplayer mpplus mp2sandbox mp2 ag ag2 mp2-brava)
+RULESETS=(classic civ2civ3 maptest multiplayer mpplus mp2sandbox mp2 ag ag2 mp2-brava mp2-caravel)
 TOPDIR="$( cd ../"$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 printf "\nThis overwrites auto-generated manual for units file, ag7.html, with\n"
@@ -24,5 +24,7 @@ printf "\nOverwriting ag27.html with ag27.bak.html\n"
    cp /vagrant/freeciv-web/src/derived/webapp/man/ag27.bak.html /vagrant/freeciv-web/src/derived/webapp/man/ag27.html
 printf "\nOverwriting mp2-brava7.html with mp2-brava7.bak.html\n"
    cp /vagrant/freeciv-web/src/derived/webapp/man/mp2-brava7.bak.html /vagrant/freeciv-web/src/derived/webapp/man/mp2-brava7.html
+printf "\nOverwriting mp2-caravel7.html with mp2-caravel7.bak.html\n"
+   cp /vagrant/freeciv-web/src/derived/webapp/man/mp2-caravel7.bak.html /vagrant/freeciv-web/src/derived/webapp/man/mp2-caravel7.html
 
 printf "\n*** REMINDER:  run ./build.sh to refresh and activate new manual.\n"
