b28b4d283e2ad664bc0beb04e12f0e9a5039492a
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2021-03-19 09:38:06 -0700

    Ruleset generation clean-up scripts.

--- freeciv-web/vclean-rules.sh
+++ freeciv-web/vclean-rules.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+11#!/bin/bash
 # builds Freeciv-web, copies the war file to Tomcat and builds the selected rulesets.
 
 RULESETS=(classic civ2civ3 maptest multiplayer mpplus mp2sandbox mp2 ag ag2 mp2-brava mp2-caravel)
 TOPDIR="$( cd ../"$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
-printf "\n./clean-rules.sh: DEPLOYED SERVER VERSION. Use ./vclean-rules.sh for vagrant installations.\n"
+printf "\n./vclean-rules.sh: VAGRANT VERSION. Use ./clean-rules.sh for deployed servers.\n"
 
 printf "\nThis overwrites the auto-generated manuals with the release-version manuals.\n"
 printf "Release versions reduce verbosity and add more information.\n"
@@ -14,15 +14,15 @@ printf "sure to make a NEW <rulesetname>#.bak.html before running this script!\n
 printf "Do a diff with src/derived/webapp/man/<rulesetname>#.html.\n"
 printf "****************************************************************************\n"
 
-printf "\nOverwriting auto-generated manual with release-version: MP2-Avant Garde"
-   cp  ~/freeciv-web/freeciv-web/src/derived/webapp/man/ag7.bak.html ~/freeciv-web/freeciv-web/src/derived/webapp/man/ag7.html
-printf "\nOverwriting auto-generated manual with release-version: MP2-Brava"
-   cp ~/freeciv-web/freeciv-web/src/derived/webapp/man/mp2-brava7.bak.html ~/freeciv-web/freeciv-web/src/derived/webapp/man/mp2-brava7.html
+printf "\nOverwriting generated manual with release-version: MP2-Avant Garde"
+   cp /vagrant/freeciv-web/src/derived/webapp/man/ag7.bak.html /vagrant/freeciv-web/src/derived/webapp/man/ag7.html
+printf "\nOverwriting generated manual with release-version: MP2-Brava"
+   cp /vagrant/freeciv-web/src/derived/webapp/man/mp2-brava7.bak.html /vagrant/freeciv-web/src/derived/webapp/man/mp2-brava7.html
 printf "\nOverwriting auto-generated manual with release-version: MP2-Caravel"
-   cp ~/freeciv-web/freeciv-web/src/derived/webapp/man/mp2-caravel6.bak.html ~/freeciv-web/freeciv-web/src/derived/webapp/man/mp2-caravel6.html
-   cp ~/freeciv-web/freeciv-web/src/derived/webapp/man/mp2-caravel7.bak.html ~/freeciv-web/freeciv-web/src/derived/webapp/man/mp2-caravel7.html
+   cp /vagrant/freeciv-web/src/derived/webapp/man/mp2-caravel6.bak.html /vagrant/freeciv-web/src/derived/webapp/man/mp2-caravel6.html
+   cp /vagrant/freeciv-web/src/derived/webapp/man/mp2-caravel7.bak.html /vagrant/freeciv-web/src/derived/webapp/man/mp2-caravel7.html
 
 printf "\nDirectly copying rules to Tomcat webapp\n"
-   cp ~/freeciv-web/freeciv-web/src/derived/webapp/man/*.* /var/lib/tomcat8/webapps/freeciv-web/man/
+   cp /vagrant/freeciv-web/src/derived/webapp/man/*.* /var/lib/tomcat8/webapps/freeciv-web/man/
 
 printf "\n*** REMINDER:  run ./build.sh to refresh and activate new manual.\n"
