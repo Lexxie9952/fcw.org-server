352def014c2ed400e27f9a8ca6284764d0458aff
Marko Lindqvist <cazfi74@gmail.com>
2021-03-26 18:52:00 +0200

    Disable threaded AI module
    You don't want it. I'd be surprised if anyone has used
    it despite the support having been built in to the server.
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/prepare_freeciv.sh
+++ freeciv/prepare_freeciv.sh
@@ -33,5 +33,5 @@ fi
 ( cd freeciv
   ./autogen.sh --no-configure-run --disable-nls
   ./configure CFLAGS="-O3" \
-              --enable-mapimg=magickwand --with-project-definition=../freeciv-web.project --enable-fcweb --enable-json --disable-delta-protocol --disable-nls --disable-fcmp --enable-freeciv-manual --disable-ruledit --enable-fcdb=no --enable-ai-static=classic,threaded --prefix=${HOME}/freeciv/ && make -s -j$(nproc)
+              --enable-mapimg=magickwand --with-project-definition=../freeciv-web.project --enable-fcweb --enable-json --disable-delta-protocol --disable-nls --disable-fcmp --enable-freeciv-manual --disable-ruledit --enable-fcdb=no --enable-ai-static=classic --prefix=${HOME}/freeciv/ && make -s -j$(nproc)
 )
