ccdd8ceb002b07aeb5335d15dc6fea33f416e9e1
Marko Lindqvist <cazfi74@gmail.com>
2021-05-28 04:00:18 +0300

    Bring in 3 blank user actions
    hrm Feature #852112
    Freeciv.org commit id: fce7b3f74834e05917a1bf1e9a8e2279fca9c1a8
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/freeciv/server/savegame/savegame3.c
+++ freeciv/freeciv/server/savegame/savegame3.c
@@ -6086,6 +6086,9 @@ static bool sg_load_player_unit(struct loaddata *loading,
           case ACTION_TRANSPORT_DISEMBARK2:
           case ACTION_TRANSPORT_BOARD:
           case ACTION_TRANSPORT_EMBARK:
+          case ACTION_USER_ACTION1:
+          case ACTION_USER_ACTION2:
+          case ACTION_USER_ACTION3:
           case ACTION_SPY_ATTACK:
           case ACTION_COUNT:
             /* None of these can take a sub target. */
