89353a7972027b12c89ab3cb698eb9199083fb5d
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2021-07-11 19:36:40 -0700

    TEST CANDIDATE.  Update UI elements affected by a change  to unit's state.

--- freeciv-web/src/main/webapp/javascript/packhand.js
+++ freeciv-web/src/main/webapp/javascript/packhand.js
@@ -1237,15 +1237,33 @@ function handle_unit_packet_common(packet_unit)
 
   update_tile_unit(units[packet_unit['id']]);
 
-  if (current_focus.length > 0 && current_focus[0]['id'] == packet_unit['id']) {
-    update_active_units_dialog();
-    update_unit_order_commands();
+  /* Update UI elements affected by a change to this unit's state:
+     1. If unit is done moving, advance focus. 
+     2. If focused on a new unit, update orders buttons and active units dialog.
+     3. and/or if ANY unit on the tile was the packet unit that underwent state
+        change, refresh the active units dialog. 
+     TODO: this probably removes the need for numerous hacky update_active_units_dialog()
+     calls that are sent, often with a timeout, after sending a packet for the unit to
+     do an action.
 
-    if (current_focus[0]['done_moving'] != packet_unit['done_moving']) {
-      update_unit_focus();
+     9July2021. This is a test candidate and may be changed/removed if testing reveals
+                suboptimal results. */
+  if (current_focus.length > 0) {
+    if (current_focus[0]['id'] == packet_unit['id']) {
+      if (current_focus[0]['done_moving'] != packet_unit['done_moving']) {
+        update_unit_focus();
+        update_unit_order_commands();
+        //update_active_units_dialog(); // this will always happen in block below
+      }
+    }
+    var tunits = tile_units(index_to_tile(current_focus[0]['tile']));
+    for (i = 0; i < tunits.length; i++) { 
+      if (tunits[i]['id'] == packet_unit['id']) {
+        update_active_units_dialog();
+        break;
+      }
     }
   }
-
   /* TODO: update various dialogs and mapview. */
 }
 
