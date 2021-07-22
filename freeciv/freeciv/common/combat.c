32154e3528256246828a4ffee5b06068816aa902
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2021-07-22 14:06:20 -0700

    Clean up and re-comment combat.c:unit_attack_all_at_tile_result()
    Flag possible bug divergence between fcw and upstream
    
    Co-Authored-By: Sveinung Kvilhaugsvik <6710184+kvilhaugsvik@users.noreply.github.com>

--- freeciv/freeciv/common/combat.c
+++ freeciv/freeciv/common/combat.c
@@ -162,9 +162,11 @@ enum unit_attack_result unit_attack_unit_at_tile_result(const struct unit *punit
 /*******************************************************************//**
   When unreachable_protects setting is TRUE:
   To attack a stack, unit must be able to attack every unit there (not
-  including transported units and UTYF_NEVER_PROTECTS defender units.)
+  including transported units and UTYF_NEVER_PROTECTS units).
   (UTYF_NEVER_BLOCKED attacker units only require one reachable unit
-  on the stack.)
+  on the stack.) TODO: Upstream seems divergent in a possibly buggy
+  way for some cases, we should figure out if there is a functional
+  result difference between forks and and resolve it.
 ************************************************************************/
 static enum unit_attack_result unit_attack_all_at_tile_result(const struct unit *punit,
                                                               const struct tile *ptile)
@@ -175,31 +177,26 @@ static enum unit_attack_result unit_attack_all_at_tile_result(const struct unit 
     /* We don't count transported units. This prevents bugs
      * like a Carrier carrying a Fighter being invulnerable. */
     if (!unit_transported(aunit)) {
-        enum unit_attack_result result;
+      enum unit_attack_result result;
 
-        result = unit_attack_unit_at_tile_result(punit, aunit, ptile);
-        /* Check for NeverProtects and NeverBlocked exceptions for an 
-           unreachable unit. Continue iteration IFF it's an exception */
+      result = unit_attack_unit_at_tile_result(punit, aunit, ptile);
+      /* Check for NeverProtects and NeverBlocked exceptions for an 
+          unreachable unit. Continue iteration IFF it's an exception */
 
-/*
-        notify_conn(NULL, NULL, E_SETTING, ftc_any,
-                _("Attack attempt! NEVER_BLOCKED=="
-                  "'%d'"), unit_has_type_flag(punit, UTYF_NEVER_BLOCKED));
-*/
-        if (result == ATT_UNREACHABLE 
-            && ( unit_has_type_flag(aunit, UTYF_NEVER_PROTECTS)
-                 || unit_has_type_flag(punit, UTYF_NEVER_BLOCKED) ) ) {
-          /* Unreachable but NOT prevented from attacking other units */
-          continue;
-        /* If defender was not an "unreachable exception", then we 
-           report cases of an illegal attack and quit */   
-        } else if (result != ATT_OK) {
-          return result;
-        }
-        /* We only arrive here if ATT_OK, meaning that at least 
-           one of the units was reachable */  
-        any_reachable_unit = TRUE;
+      if (result == ATT_UNREACHABLE
+          && ( unit_has_type_flag(aunit, UTYF_NEVER_PROTECTS)
+                || unit_has_type_flag(punit, UTYF_NEVER_BLOCKED) ) ) {
+        /* Unreachable but NOT prevented from attacking other units */
+        continue;
+      /* If defender was not an "unreachable exception", then we 
+          report cases of an illegal attack and quit */   
+      } else if (result != ATT_OK) {
+        return result;
       }
+      /* We only arrive here if ATT_OK, meaning that at least 
+          one of the units was reachable */  
+      any_reachable_unit = TRUE;
+    }
   } unit_list_iterate_end;
 
   /* If there are only unreachable units, we have to return 
