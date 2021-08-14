457c3d575b8ad6d7101eaf8361604bc04799524d
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2021-08-14 05:23:17 -0700

    Improve title text in game status panel

--- freeciv-web/src/main/webapp/javascript/game.js
+++ freeciv-web/src/main/webapp/javascript/game.js
@@ -108,9 +108,11 @@ function update_game_status_panel() {
   if (C_S_RUNNING != client_state() ) return;
 
   var status_html = "";
-
+  var msg_title_prefix = unread_messages > 0 ? "Unread messages.&#013;&#010;&#013;&#010;Click = " : ""
   // unread message counter with toggle message window
-  status_html = "<span onclick='toggle_msgbox();' style='cursor: pointer;' title='Unread messages'><i class='fa fa-commenting-o' aria-hidden='true'></i>"
+
+  status_html = "<span onclick='toggle_msgbox();' style='cursor: pointer;' title='"+msg_title_prefix
+              + "Show/hide message window.'><i class='fa fa-commenting-o' aria-hidden='true'></i>"
               + "<font color='#ff8080'>&nbsp;<b>"
               + ((unread_messages>0) ? unread_messages : "")
               +"</b>&nbsp;&nbsp;&nbsp;</font></span>";
@@ -168,10 +170,10 @@ function update_game_status_panel() {
     }
     status_html += "</span>";
 
-    if (!is_small_screen()) status_html += "<span style='cursor:pointer;' onclick='javascript:request_report(2)'>"; // type 2 is demographics
-    if (!is_small_screen()) status_html += "&nbsp; <i style='color:#ffc8c0' class='fa fa-child' aria-hidden='true' title='Total Citizens in all Cities'></i> ";
+    if (!is_small_screen()) status_html += "<span style='cursor:pointer;' title='Total Citizens in all Cities.&#013;&#010;&#013;&#010;Click = Demographics Report' onclick='javascript:request_report(2)'>"; // type 2 is demographics
+    if (!is_small_screen()) status_html += "&nbsp; <i style='color:#ffc8c0' class='fa fa-child' aria-hidden='true'></i> ";
     if (!is_small_screen()) status_html += "<b>" + city_size_sum(client.conn.playing.playerno) + "</b>  &nbsp;&nbsp;";
-    if (!is_small_screen()) status_html += "<i style='color:#d8dff0' class='fa fa-clock-o' aria-hidden='true' title='Year (turn)'></i> <b>" + get_year_string() + "</b> &nbsp;&nbsp;</span>";
+    if (!is_small_screen()) status_html += "<i style='color:#d8dff0' class='fa fa-clock-o' aria-hidden='true' title='Year (turn)&#013;&#010;&#013;&#010;Click = Demographics Report'></i> <b title='Year (turn)&#013;&#010;&#013;&#010;Click = Demographics Report'>" + get_year_string() + "</b> &nbsp;&nbsp;</span>";
     status_html += "<i style='color:#89c06a' class='fa fa-money' aria-hidden='true' title='Treasury'></i> ";
    
     var income_color = "<b";
@@ -180,14 +182,14 @@ function update_game_status_panel() {
     else if (pplayer['expected_income'] > 0) income_color += income_calculated_by_client 
        ? " style='color:#89c06a' title='Income'" : " style='color:#a2b095' title='Income'" // slight hint whether accurate client calc or from server.
     
-    status_html += "<b style='color:#ffde80; cursor:default;' title='Gold'>"+pplayer['gold'] 
+    status_html += "<b style='color:#ffde80; cursor:default;' title='Gold reserves'>"+pplayer['gold'] 
     + "</b> "+income_color+" style='cursor:default;'>" + net_income + "</b>"+"  &nbsp;&nbsp;";
-    status_html += "<span style='cursor:pointer;' onclick='javascript:show_tax_rates_dialog();'><i style='color:#ebb445' class='fa fa-btc' aria-hidden='true' title='Tax rate'></i> <b style='color:#fff0d1'>" 
-    + tax + "</b><span style='color:#bcbcbc'>%</span> &nbsp;";
-    status_html += "<i style='color:#b087c5;' class='fa fa-music' aria-hidden='true' title='Luxury rate'></i> <b style='color:#f5e8ff'>" + lux + "</b><span style='color:#bcbcbc'>%</span> &nbsp;";
+    status_html += "<span style='cursor:pointer;' onclick='javascript:show_tax_rates_dialog();'><i style='color:#ebb445' class='fa fa-btc' aria-hidden='true' title='Tax rate'></i> <b title='Tax rate' style='color:#fff0d1'>" 
+    + tax + "</b><span title='Tax rate' style='color:#bcbcbc'>%</span> &nbsp;";
+    status_html += "<i style='color:#b087c5;' class='fa fa-music' aria-hidden='true' title='Luxury rate'></i> <b title='Luxury rate' style='color:#f5e8ff'>" + lux + "</b><span title='Luxury rate' style='color:#bcbcbc'>%</span> &nbsp;";
     const sci_title = "Science:\n"+(techs[client.conn.playing['researching']]===undefined ? "" : techs[client.conn.playing['researching']]['name']) +"\n"
                     + client.conn.playing['bulbs_researched'] + " / " + client.conn.playing['researching_cost'] +"\n" + bulb_output_text;             
-    status_html += "<i style='color:#a8ccd7;'class='fa fa-flask' aria-hidden='true' title='"+sci_title+"'></i> <b style='color:#ebfaff'>" + sci + "</b><span style='color:#bcbcbc'>%</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ";
+    status_html += "<i style='color:#a8ccd7;'class='fa fa-flask' aria-hidden='true' title='"+sci_title+"'></i> <b title='"+sci_title+"' style='color:#ebfaff'>" + sci + "</b><span title='"+sci_title+"' style='color:#bcbcbc'>%</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ";
   
   } else if (server_settings != null && server_settings['metamessage'] != null) {    // Status message for gamemasters/admins/supercows:
     status_html += "Observing - Turn <b>" + game_info['turn'] + "</b> -";
