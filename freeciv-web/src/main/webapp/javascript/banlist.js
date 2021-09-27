278aabb3cb0c065c7cc51b82518e76f44a6893f6
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2021-09-26 17:42:51 -0700

    MP2D

--- freeciv-web/src/main/webapp/javascript/banlist.js
+++ freeciv-web/src/main/webapp/javascript/banlist.js
@@ -21,7 +21,7 @@
 /* This is a list of banned users of Freeciv-web.
    Note that user accounts can also be disabled by setting activated=0 in the auth DB table.
 */
-var banned_users = ["Rayray", "Blank", "DMX", "empmarkus", "Vladimir"];
+var banned_users = ["Blank", "DMX", "Canik"];
 
 /**************************************************************************
  Returns false if the text contains a banned user.
