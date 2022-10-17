7ecec5fb14349b29f9f94d9017e09a2a81f0da53
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2022-10-16 23:12:45 -0700

    Update coredumpon.sh

--- freeciv-web/coredumpon.sh
+++ freeciv-web/coredumpon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # builds javascript files Freeciv-web and copies the resulting file to tomcat.
 
+printf "\nPrior to running this script, do the command:\nsudo -i"
+
 ulimit -c unlimited
 sudo bash -c 'echo /tmp/core/core.%e.%p.%h.%t > /proc/sys/kernel/core_pattern'
