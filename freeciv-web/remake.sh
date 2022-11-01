f161b22c0702f0bbc748858e1df7884fa050cd42
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2022-10-31 20:32:27 -0700

    Add more comment docs to ./remake.sh

--- freeciv-web/remake.sh
+++ freeciv-web/remake.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Does EVERY step to re-build C-server and restart freeciv-web.
+# Assumes development environment and produces debug info for coredumps.
+# Use ./remake-nodebug.sh for no debug/coredump make.
 
 printf "\n**********************************************************************\n"
 printf "./remake.sh: DEPLOYED SERVER version. Re-builds server executables.\n"
+printf "Use ./remake-nodebug.sh to suppress debug/coredump make.\n"
+printf "Use ../freeciv/./prepare.sh for complete rebuild.\n"
 printf "Important: Use ./vremake.sh for vagrant installations.\n"
 printf "**********************************************************************\n"
 
