350ccfe5ddd073060ba87779418a0a7a952b0245
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2023-12-11 17:01:57 -0800

    Script 2.
    fcw-multipass.sh script
    Clean up and put in warnings for disabling VPN before execution.

--- fcw-multipass.sh
+++ fcw-multipass.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
 destroy() {
-  echo "DESTROYING EXISTING FcW Virtual machine"
+  echo "**********************************************************"
+  echo "!!! REMEMBER TO DISABLE VPN BEFORE RUNNING THIS SCRIPT !!!"
+  echo "**********************************************************"
+  echo "!!! REMEMBER TO DISABLE VPN BEFORE RUNNING THIS SCRIPT !!!"
+  echo "**********************************************************"
+  echo "!!! REMEMBER TO DISABLE VPN BEFORE RUNNING THIS SCRIPT !!!"
+  echo "**********************************************************"
+  echo ""
+  echo "Destroying any existing fcw virtual machine..."
   multipass delete fcw
   multipass purge
-  echo "DO NOT WORRY IF THIS FAILS it means that you did not have a FcW Virtual Machine running"
+  echo "(This harmlessly fails if no fcw virtual machine running)"
 }
 
 create() {
   multipass launch bionic -n fcw --memory 3G --disk 20G --cpus 2 --mount $(pwd):/home/ubuntu/freeciv-web
-  echo "======= STARTING FcW Installation ============"
+  echo ""
+  echo "================ Starting FCW Installation ================"
   multipass exec fcw --working-directory /home/ubuntu/freeciv-web -- chmod +x scripts/install/install-multipass.sh
   multipass exec fcw --working-directory /home/ubuntu/freeciv-web -- scripts/install/install-multipass.sh
-  echo "======= Installation complete ================"
-  echo "To complete the booting of FCW, please now do:"
+  echo "================ Installation complete ================"
+  echo "To finish, please execute:"
+  echo ""
   echo "multipass shell fcw"
   echo "./multipass-entrypoint.sh start"
-  echo ""
 }
 
 rebuild() {
@@ -35,5 +44,7 @@ destroy)
 *)
   rebuild
 esac
+
+echo $'\a'
 
 exit 0
