fa9cd386e2df9d59aba4ab5ee6207a2085f2bc74
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2023-12-13 07:05:00 -0800

    Create rules.sh
    script for building actively developed rulesets instead of all of them

---
+++ freeciv-web/rules.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# builds Freeciv-web, copies the war file to Tomcat and builds the selected rulesets.
+
+RULESETS=(classicplus mp2-dragoon mp2-elephant)
+TOPDIR="$( cd ../"$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+
+printf "\n**********************************************************************\n"
+printf "./build-rules.sh: DEPLOYED SERVER version of ruleset build script.\nNOTE: Use ./vbuild-rules.sh for vagrant installations.\n"
+printf "**********************************************************************\n"
+
+printf "\nUpdating rulesets...\n"
+   for r in ${RULESETS[@]}; do
+      echo "Copying $r"
+      bash ../scripts/copy-ruleset.sh $r      
+   done
+
+printf "\nRe-generating manuals\n"
+   cd src/derived/webapp/man
+   for r in ${RULESETS[@]}; do
+      echo "Generating help manual for *********************************************** $r"    
+       ${HOME}/freeciv/bin/freeciv-manual -r $r
+   done
+
+printf "\n\n****** REMINDER: ************************************************************\n"
+printf "(1) ./clean-rules.sh is required to fix custom .html files that were overwritten.\n"
+printf "(2) run ./build.sh to refresh and activate the new manuals.\n"
