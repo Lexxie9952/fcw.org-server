50d1b86b150251a1b825cd429bcc75360a07dc09
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2023-12-13 16:55:53 -0800

    Shortcut aliases for frequent build/make/compile scripts

--- freeciv-web/salias.sh
+++ freeciv-web/salias.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Set up useful aliases for server development.
+# You can't set bash aliases from an .sh script so this simply serves as a mnemonic
+# for setting them yourself manually in your alias setup. 
 # ***********************************************************************************
 
 alias fcw="cd ~/freeciv-web/freeciv-web"
 alias bjs="cd ~/freeciv-web/freeciv-web; ./build-js.sh"
-alias b="ccd ~/freeciv-web/freeciv-web; ./build.sh"
-alias rules="cd ~/freeciv-web/freeciv-web; ./build-rules.sh; ./clean-rules.sh"
+alias b="cd ~/freeciv-web/freeciv-web; ./build.sh"
 alias ball="cd ~/freeciv-web/freeciv-web; ./build-images.sh; ./build-rules*; ./clean-rules*; ./build.sh"
 alias bim="cd ~/freeciv-web/freeciv-web; ./build-images.sh"
-alias vr="cd ~/freeciv-web/freeciv; ./prepare_freeciv.sh; cd build; make install"
+alias comp="echo '==================================================================='; echo 'Recompile new FCW server executable without shutdown and restart...'; echo '==================================================================='; echo ''; cd ~/freeciv-web/freeciv; ./prepare_freeciv.sh; cd build; make install"
 echo "==== Aliases set for production server ===="
 exit
