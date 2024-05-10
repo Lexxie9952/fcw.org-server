9b3a506ecedab1fbdfd4dd5ca842aad1c0e22b1b
Lexxie9952 <32187224+Lexxie9952@users.noreply.github.com>
2024-05-10 02:41:18 -0700

    Better messaging in remake and remakem bash scripts

--- freeciv-web/remake.sh
+++ freeciv-web/remake.sh
@@ -2,20 +2,42 @@
 # Assumes deployed server environment and produces debug info for coredumps.
 # Does EVERY step to re-build C-server and restart freeciv-web.
 # Use ./remake-nodebug.sh for no debug/coredump make.
+START=$(date +%s)
+GREEN='\033[0;32m'; LGREEN='\033[1;32m'; LGREY='\033[0;37m'; DGREY='\033[1;30m'; WHITE='\033[1;37m'
+BLUE='\033[0;34m'; LBLUE='\033[1;34m'; BROWN='\033[0;33m'; YELLOW='\033[1;33m'; RED='\033[0;31m'
+PINK='\033[1;31m'; PURPLE='\033[0;35m'; MAGENTA='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'
 
-printf "\n*********************************************************************\n"
-printf "./remake.sh: DEPLOYED SERVER version. Re-builds server executables.\n"
-printf "* Use ../freeciv/./prepare.sh for complete rebuild.\n"
-printf "***********************************************************************\n"
-printf "Remake scripts for other contexts:" 
-printf "Use ./remake-nodebug.sh to suppress debug/coredump make.\n"
-printf "Use ./vremake.sh for Vagrant installations.\n"
-printf "Use ./remakem.sh for Multipass installations.\n"
-printf "***********************************************************************\n"
+echo -e "${BROWN}********************************************************************************${LGREY}"
+echo -e "${LCYAN}./remake.sh: ${YELLOW}DEPLOYED SERVER${LGREY} version."
+echo -e "${BROWN}********************************************************************************${LGREY}"
+echo -e "${BROWN}*${LGREY} Re-builds server executables for DEPLOYED SERVER installations."
+echo -e "${BROWN}*${LGREY} Use ${CYAN}../freeciv/./prepare.sh${LGREY} for complete rebuild."
+echo -e "${BROWN}********************************************************************************${LGREY}"
+echo -e "Remake scripts for other contexts:"
+echo -e "Use ${CYAN}./remake-nodebug.sh${LGREY} to suppress debug/coredump make."
+echo -e "Use ${CYAN}./remakem.sh${LGREY} for Multipass installations."
+echo -e "Use ${CYAN}./vremake.sh${LGREY} for Vagrant installations."
+echo -e "${BROWN}********************************************************************************${LGREY}"
 
 CFLAGS="-g"
 
-cd ~/freeciv-web/freeciv && ./prepare_freeciv.sh && cd build && make install && cd ../../scripts && ./stop-freeciv-web.sh && ./start-freeciv-web.sh
+pushd ~/freeciv-web/freeciv
+./prepare_freeciv.sh
+pushd build
+if make install
+then
+  echo -e "${GREEN}*********************************${WHITE} MAKE SUCCESS!!${GREEN} *******************************${LGREY}"
+else
+  echo -e "${RED}*********************************${WHITE} MAKE FAILURE!!${RED} ********************************${LGREY}"
+fi
+popd && popd
+pushd ../scripts
+./stop-freeciv-web.sh && ./start-freeciv-web.sh
+popd
 
 # let user know when it's finished
 echo $'\a'
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo -e "${LCYAN}./remake.sh${LGREY} finished in ${WHITE}${DIFF}${LGREY} seconds."
+exit 0
--- freeciv-web/remakem.sh
+++ freeciv-web/remakem.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 # Assumes MULTIPASS environment and produces debug info for coredumps.
 # Does EVERY step to re-build C-server and restart freeciv-web.
+START=$(date +%s)
+GREEN='\033[0;32m'; LGREEN='\033[1;32m'; LGREY='\033[0;37m'; DGREY='\033[1;30m'; WHITE='\033[1;37m'
+BLUE='\033[0;34m'; LBLUE='\033[1;34m'; BROWN='\033[0;33m'; YELLOW='\033[1;33m'; RED='\033[0;31m'
+PINK='\033[1;31m'; PURPLE='\033[0;35m'; MAGENTA='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'
 
-GREEN='\033[0;32m'
-LGREEN='\033[1;32m'
-LGREY='\033[0;37m'
-DGREY='\033[1;30m'
-WHITE='\033[1;37m'
-BLUE='\033[0;34m'
-LBLUE='\033[1;34m'
-BROWN='\033[0;33m'
-YELLOW='\033[1;33m'
-RED='\033[0;31m'
-PINK='\033[1;31m'
-PURPLE='\033[0;35m'
-MAGENTA='\033[1;35m'
-CYAN='\033[0;36m'
-LCYAN='\033[1;36m'
-
-echo -e "${YELLOW}************************************************************************************${LGREY}"
-echo -e "${LCYAN}./remakem.sh: ${LGREY}MULTIPASS SERVER version."
-echo -e "${YELLOW}************************************************************************************${LGREY}"
-echo -e "* Re-builds server executables for MULTIPASS virtual machine instances."
-echo -e "* Use ../freeciv/./prepare.sh for complete rebuild."
-echo -e "${YELLOW}************************************************************************************${LGREY}"
+echo -e "${YELLOW}********************************************************************************${LGREY}"
+echo -e "${LCYAN}./remakem.sh: ${WHITE}MULTIPASS SERVER${LGREY} version."
+echo -e "${YELLOW}********************************************************************************${LGREY}"
+echo -e "${YELLOW}*${LGREY} Re-builds server executables for MULTIPASS VM instances."
+echo -e "${YELLOW}*${LGREY} Use ${CYAN}../freeciv/./prepare.sh${LGREY} for complete rebuild."
+echo -e "${YELLOW}********************************************************************************${LGREY}"
 echo -e "Remake scripts for other contexts:"
 echo -e "Use ${CYAN}./remake.sh${LGREY} on deployed server installations."
 echo -e "Use ${CYAN}./vremake.sh${LGREY} for Vagrant installations."
-echo -e "${YELLOW}************************************************************************************${LGREY}"
+echo -e "${YELLOW}********************************************************************************${LGREY}"
 
 CFLAGS="-g"
 
@@ -55,9 +43,9 @@ build() {
   pushd build
   if make install
   then
-    echo -e "${GREEN}*********************************${WHITE} BUILD SUCCESS!!${GREEN} ********************************${LGREY}"
+    echo -e "${GREEN}*********************************${WHITE} MAKE SUCCESS!!${GREEN} *******************************${LGREY}"
   else
-    echo -e "${RED}*********************************${WHITE} BUILD FAILURE!!${RED} *********************************${LGREY}"
+    echo -e "${RED}*********************************${WHITE} MAKE FAILURE!!${RED} ********************************${LGREY}"
   fi
 
   popd && popd
@@ -71,5 +59,7 @@ start
 
 # let user know when it's finished
 echo $'\a'
-
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo -e "${LCYAN}./remakem.sh${LGREY} finished in ${WHITE}${DIFF}${LGREY} seconds."
 exit 0
